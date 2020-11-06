9d9471b39dbe7299b090f372a52ad05f553af447
jose.rubio5317@alumnos.udg.mx <jose.rubio5317@alumnos.udg.mx>
2020-11-05 21:39:51 -0600

    Sobrecargamos el operador menor que para poder comparar dos objetos de la clase computadora, con el fin de poder ordenar

---
+++ computadora.h
@@ -0,0 +1,66 @@
+#ifndef COMPUTADORA_H
+#define COMPUTADORA_H
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+class Computadora{
+    private:
+        string sistemaOperativo;
+        int tamanoRam;
+        string procesador;
+        string nombreEquipo;
+    public:
+        Computadora();
+        Computadora(const string &sistemaOperativo,int tamanoRam,const string &procesador,const string &nombreEquipo);
+
+        void setSistemaOperativo(const string &v);
+        void setTamanoRam(int v);
+        void setProcesador(const string &v);
+        void setNombreEquipo(const string &v);
+
+        string getSistemaOperativo();
+        int getTamanoRam();
+        string getProcesador();
+        string getNombreEquipo();
+
+        friend ostream& operator << (ostream &out, const Computadora &c){
+            out << left;
+            out << setw(16) << c.nombreEquipo;
+            out << setw(20) << c.sistemaOperativo;
+            out << setw(6) << c.tamanoRam;
+            out << setw(16) << c.procesador;
+            out << endl;
+
+            return out;
+        }
+        friend istream& operator >> (istream &in, Computadora &c){
+            cout << "Nombre del Equipo: ";
+            getline(cin, c.nombreEquipo);
+            cout << "Sistema Operativo: ";
+            getline(cin, c.sistemaOperativo);
+            cout << "Tamano de Ram: ";
+            cin >> c.tamanoRam;
+            cout << "Microprocesador: ";
+            cin.ignore();
+            getline(cin, c.procesador);
+
+            return in;
+        }
+
+        bool operator==(const Computadora& c){
+            return nombreEquipo == c.nombreEquipo;
+        }
+        bool operator==(const Computadora& c) const {
+            return nombreEquipo == c.nombreEquipo;
+        }
+        bool operator<(const Computadora& c){
+            return nombreEquipo < c.nombreEquipo;
+        }
+        bool operator<(const Computadora& c) const {
+            return nombreEquipo < c.nombreEquipo;
+        }
+};
+
+#endif
